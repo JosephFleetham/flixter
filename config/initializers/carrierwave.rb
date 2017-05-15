24c8d0195205c1bb33370d6e4d32c9887a83eb3b
JosephFleetham <joseph.fleetham@gmail.com>
2017-05-15 23:00:09 +0000

    Student Enrollment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   if Rails.env.development?
   config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
   config.root = '/home/vagrant/uploads_tmp/tmp'
-
+end
 end
